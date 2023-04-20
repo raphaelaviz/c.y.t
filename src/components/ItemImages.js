976e33c6a6ac83177d1cc6d5b025f53f647f9a82
rapharrrr <125589553+rapharrrr@users.noreply.github.com>
2023-04-20 12:35:06 +0100

    Adjusting comment line

--- src/components/ItemImages.js
+++ src/components/ItemImages.js
@@ -4,12 +4,12 @@ import React from 'react';
 // This component is responsible for rendering the images of items 
 // that haven't been collected yet within the game grid.
 
-// Iterates through the currentCells array to render each item not assigned as "collected".
 
 const ItemImages = ({ currentCells }) => {
   return (
-
-
+    
+    // Iterates through the currentCells array to render each item not assigned as "collected".
+    
    <>
       {currentCells.map((cell, index) => {
         if (cell.item && !cell.collected) {
